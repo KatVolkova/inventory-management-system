d63fae15042652662e1bb6e2b94bb8f111f5d79e
Ekaterina Volkova <volkovakatrin87@yahoo.com>
2024-12-14 17:57:19 +0000

    Update settings.py

--- ims_project/settings.py
+++ ims_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$&#lm034pe*18$cdxb+0byv7=f$fn9_r5g=@i+(!)5!cf6rjh'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
