48eedc8b3df1b928ebaddcebe4c176a415986270
Ekaterina Volkova <volkovakatrin87@yahoo.com>
2024-12-14 18:46:05 +0000

    Add an Item model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    sku = models.CharField(max_length=50, unique=True)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
