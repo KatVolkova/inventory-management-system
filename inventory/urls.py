57cfd97617f8a72a836b530ec411649b8d67a76b
Ekaterina Volkova <volkovakatrin87@yahoo.com>
2024-12-19 17:29:13 +0000

    Add edit and delete functionality for items

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.ItemListView.as_view(), name="home"),
     path("item/<int:pk>/", views.ItemDetailView.as_view(), name="item-detail"),
+    path("item/<int:pk>/edit/", views.ItemEditView.as_view(), name="item-edit"),
+    path("item/<int:pk>/delete/", views.ItemDeleteView.as_view(), name="item-delete"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -14,3 +17,31 @@ class ItemDetailView(generic.DetailView):
     model = Item
     template_name = "inventory/item_detail.html"
     context_object_name = "item"
+
+
+class ItemEditView(View):
+    def get(self, request, pk):
+        item = get_object_or_404(Item, pk=pk)
+        form = ItemForm(instance=item)
+        return render(request, "inventory/item_edit.html", {"form": form, "item": item})
+
+
+    def post(self, request, pk):
+        item = get_object_or_404(Item, pk=pk)
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Item updated successfully!")
+            return HttpResponseRedirect(reverse("item-detail", args=[pk]))
+        return render(request, "inventory/item_edit.html", {"form": form, "item": item})
+
+
+
+
+
+class ItemDeleteView(View):
+    def post(self, request, pk):
+        item = get_object_or_404(Item, pk=pk)
+        item.delete()
+        messages.success(request, "Item deleted successfully!")
+        return HttpResponseRedirect(reverse("home"))
