ef4908bddd9ee6e429ff3573cf63d21aa9698b9d
Ekaterina Volkova <volkovakatrin87@yahoo.com>
2025-01-03 21:53:03 +0000

    Move tables related js code to a separate js folder

---
+++ static/js/tables.js
@@ -0,0 +1,112 @@
+/* jshint esversion: 6 */
+/* jshint jquery: true */
+$(document).ready(function() {
+    // Initialize Stock Report Table
+    $('#stockReportTable').DataTable({
+    dom: 'Bfrtip', 
+    buttons: [
+    {
+        extend: 'copy',
+        text: '<i class="fas fa-copy"></i> Copy',
+        className: 'btn btn-primary'
+    },
+    {
+        extend: 'csv',
+        text: '<i class="fas fa-file-csv"></i> CSV',
+        className: 'btn btn-primary'
+    },
+    {
+        extend: 'excel',
+        text: '<i class="fas fa-file-excel"></i> Excel',
+        className: 'btn btn-primary'
+    },
+    {
+        extend: 'pdf',
+        text: '<i class="fas fa-file-pdf"></i> PDF',
+        className: 'btn btn-primary'
+    },
+    {
+        extend: 'print',
+        text: '<i class="fas fa-print"></i> Print',
+        className: 'btn btn-primary'
+    }
+    ],
+    footerCallback: function (row, data, start, end, display) {
+    // Calculate Totals
+    var api = this.api();
+
+    // Total Quantity
+    var totalQuantity = api
+        .column(1, { page: 'current' })
+        .data()
+        .reduce(function (a, b) {
+            return parseInt(a) + parseInt(b);
+        }, 0);
+
+    // Total Value
+    var totalValue = api
+        .column(2, { page: 'current' })
+        .data()
+        .reduce(function (a, b) {
+            return parseFloat(a) + parseFloat(b.replace(/[\$,]/g, ''));
+        }, 0);
+
+    // Average Price
+    var averagePrice =
+        totalValue / api.column(4, { page: 'current' }).data().length || 0;
+
+    // Summary row
+    $(api.column(1).footer()).html(totalQuantity);
+    $(api.column(2).footer()).html('$' + totalValue.toFixed(2));
+    $(api.column(4).footer()).html('$' + averagePrice.toFixed(2));
+}
+});
+
+// Highlight rows based on the 'Items Below Stock Threshold' column
+$('#stockReportTable tbody tr').each(function () {
+var lowStock = $(this).find('td').eq(5).text(); 
+if (parseInt(lowStock) > 0) {
+    $(this).addClass('table-danger'); 
+}
+});
+// Initialize Low Stock Items Table
+$('#lowStockItemsTable').DataTable({
+dom: 'Bfrtip',
+buttons: [
+    {
+        extend: 'copy',
+        text: '<i class="fas fa-copy"></i> Copy',
+        className: 'btn btn-primary'
+    },
+    {
+        extend: 'csv',
+        text: '<i class="fas fa-file-csv"></i> CSV',
+        className: 'btn btn-primary'
+    },
+    {
+        extend: 'excel',
+        text: '<i class="fas fa-file-excel"></i> Excel',
+        className: 'btn btn-primary'
+    },
+    {
+        extend: 'pdf',
+        text: '<i class="fas fa-file-pdf"></i> PDF',
+        className: 'btn btn-primary'
+    },
+    {
+        extend: 'print',
+        text: '<i class="fas fa-print"></i> Print',
+        className: 'btn btn-primary'
+    }
+],
+paging: true, 
+searching: true, 
+ordering: true, 
+order: [[2, 'asc']], 
+columnDefs: [
+    { targets: 4, orderable: false } 
+]   
+});
+// Initialize Bootstrap tooltips
+$('[title]').tooltip();
+});
