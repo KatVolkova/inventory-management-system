4111f1ed42e8b061b60b4ee86125c62d9882ea44
Ekaterina Volkova <volkovakatrin87@yahoo.com>
2025-01-07 01:21:01 +0000

    Improve accessability indicators for stock control and manage inventory pages

--- staticfiles/js/tables.js
+++ staticfiles/js/tables.js
@@ -8,27 +8,42 @@ $(document).ready(function() {
     {
         extend: 'copy',
         text: '<i class="fas fa-copy"></i> Copy',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Copy table data to clipboard'
+        }
     },
     {
         extend: 'csv',
         text: '<i class="fas fa-file-csv"></i> CSV',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Download table data as CSV file'
+        }
     },
     {
         extend: 'excel',
         text: '<i class="fas fa-file-excel"></i> Excel',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Download table data as Excel file'
+        }
     },
     {
         extend: 'pdf',
         text: '<i class="fas fa-file-pdf"></i> PDF',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Download table data as PDF file'
+        }
     },
     {
         extend: 'print',
         text: '<i class="fas fa-print"></i> Print',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Print table data'
+        }
     }
     ],
     footerCallback: function (row, data, start, end, display) {
@@ -56,9 +71,9 @@ $(document).ready(function() {
         totalValue / api.column(4, { page: 'current' }).data().length || 0;
 
     // Summary row
-    $(api.column(1).footer()).html(totalQuantity);
-    $(api.column(2).footer()).html('$' + totalValue.toFixed(2));
-    $(api.column(4).footer()).html('$' + averagePrice.toFixed(2));
+    $(api.column(1).footer()).html(totalQuantity).attr('aria-live', 'polite');
+    $(api.column(2).footer()).html('$' + totalValue.toFixed(2)).attr('aria-live', 'polite');
+    $(api.column(4).footer()).html('$' + averagePrice.toFixed(2)).attr('aria-live', 'polite');
 }
 });
 
@@ -66,7 +81,7 @@ $(document).ready(function() {
 $('#stockReportTable tbody tr').each(function () {
 var lowStock = $(this).find('td').eq(5).text(); 
 if (parseInt(lowStock) > 0) {
-    $(this).addClass('table-danger'); 
+    $(this).addClass('table-danger').attr('aria-live', 'polite').attr('role', 'alert'); 
 }
 });
 // Initialize Low Stock Items Table
@@ -76,27 +91,42 @@ buttons: [
     {
         extend: 'copy',
         text: '<i class="fas fa-copy"></i> Copy',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Copy table data to clipboard'
+        }
     },
     {
         extend: 'csv',
         text: '<i class="fas fa-file-csv"></i> CSV',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Download table data as CSV file'
+        }
     },
     {
         extend: 'excel',
         text: '<i class="fas fa-file-excel"></i> Excel',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Download table data as Excel file'
+        }
     },
     {
         extend: 'pdf',
         text: '<i class="fas fa-file-pdf"></i> PDF',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Download table data as PDF file'
+        }
     },
     {
         extend: 'print',
         text: '<i class="fas fa-print"></i> Print',
-        className: 'btn btn-primary'
+        className: 'btn btn-primary',
+        attr: {
+            'aria-label': 'Print table data'
+        }
     }
 ],
 paging: true, 
@@ -105,7 +135,32 @@ ordering: true,
 order: [[2, 'asc']], 
 columnDefs: [
     { targets: 4, orderable: false } 
-]   
+], 
+language: {
+    paginate: {
+        previous: '<span aria-hidden="true">Previous</span>',
+        next: '<span aria-hidden="true">Next</span>',
+    },
+    aria: {
+        sortAscending: 'Activate to sort column ascending',
+        sortDescending: 'Activate to sort column descending'
+    } 
+    },
+    drawCallback: function () {
+        // Add ARIA attributes to pagination buttons
+        $('.paginate_button.previous').attr({
+            'aria-label': 'Go to the previous page',
+            'role': 'button',
+        });
+        $('.paginate_button.next').attr({
+            'aria-label': 'Go to the next page',
+            'role': 'button',
+        });
+        $('.paginate_button.current').attr({
+            'aria-label': 'You are on the current page',
+            'role': 'button',
+        });
+    }
 });
 // Initialize Transactions Table
 $('#transactionsTable').DataTable({
@@ -114,27 +169,42 @@ $('#transactionsTable').DataTable({
         {
             extend: 'copy',
             text: '<i class="fas fa-copy"></i> Copy',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Copy table data to clipboard'
+            }
         },
         {
             extend: 'csv',
             text: '<i class="fas fa-file-csv"></i> CSV',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Download table data as CSV file'
+            }
         },
         {
             extend: 'excel',
             text: '<i class="fas fa-file-excel"></i> Excel',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Download table data as Excel file'
+            }
         },
         {
             extend: 'pdf',
             text: '<i class="fas fa-file-pdf"></i> PDF',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Download table data as PDF file'
+            }
         },
         {
             extend: 'print',
             text: '<i class="fas fa-print"></i> Print',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Print table data'
+            }
         }
     ],
     paging: true,
@@ -152,27 +222,42 @@ $('#allItemsTable').DataTable({
         {
             extend: 'copy',
             text: '<i class="fas fa-copy"></i> Copy',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Copy table data to clipboard'
+            }
         },
         {
             extend: 'csv',
             text: '<i class="fas fa-file-csv"></i> CSV',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Download table data as CSV file'
+            }
         },
         {
             extend: 'excel',
             text: '<i class="fas fa-file-excel"></i> Excel',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Download table data as Excel file'
+            }
         },
         {
             extend: 'pdf',
             text: '<i class="fas fa-file-pdf"></i> PDF',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Download table data as PDF file'
+            }
         },
         {
             extend: 'print',
             text: '<i class="fas fa-print"></i> Print',
-            className: 'btn btn-primary'
+            className: 'btn btn-primary',
+            attr: {
+                'aria-label': 'Print table data'
+            }
         }
     ],
     paging: true,
@@ -182,5 +267,13 @@ $('#allItemsTable').DataTable({
 });
 
 // Initialize Bootstrap tooltips
-$('[title]').tooltip();
+$('[title]').tooltip(
+    {
+        container: 'body',
+        trigger: 'hover focus',
+        placement: 'top'
+    }).attr('aria-describedby', function () {
+        return $(this).attr('id') + '-tooltip';
+    }
+);
 });
